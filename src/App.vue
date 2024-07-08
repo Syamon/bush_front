7e38d01d5c35882d7145ba44bc34f9506f3f00f7
Syamon <s.truskalo@yandex.ru>
2024-07-09 01:16:20 +0300

    первый комит

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderComponent/>
   <router-view/>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 
+export default defineComponent({
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+})
+</script>
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #e1f2f7;
+  height: 100vh;
 }
 
 nav {
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+    </nav>
+</template>
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+export default class HeaderComponent extends Vue {}
+</script>
